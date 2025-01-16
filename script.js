0673a4bac047d32dd1da1000325df3d9934ddbc8
DamianGaming <116504166+DamianGaming@users.noreply.github.com>
2025-01-16 18:35:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ window.addEventListener('scroll', function () {
     footer.classList.remove('show');  // Hide footer when not at the bottom
   }
 });
+// Toggle the "Read More" functionality for the news section
+function toggleNews() {
+  var extraNews = document.getElementById("extraNews");
+  var readMoreBtn = document.getElementById("readMoreBtn");
+  
+  if (extraNews.style.display === "none") {
+    extraNews.style.display = "inline";
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    extraNews.style.display = "none";
+    readMoreBtn.innerText = "Read More";
+  }
+}
+
+// Toggle the "More Details" functionality for each update log
+function toggleLogDetails(logId) {
+  var logDetails = document.getElementById(logId);
+  
+  if (logDetails.style.display === "none") {
+    logDetails.style.display = "block";
+  } else {
+    logDetails.style.display = "none";
+  }
+}
